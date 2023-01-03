829c8a7a356d43e22c970da080c53c735bc2e11b
J. Sebastian Paez <jspaezp@users.noreply.github.com>
2023-01-02 19:49:30 -0600

    added hparam optimization

---
+++ benchmarking/hparam_tune.zsh
@@ -0,0 +1,29 @@
+#!/usr/bin/zsh
+
+set -x
+set -e
+
+# Copied shamelessly from stackoverflow
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+NUM_WORKERS=2
+
+wandb sweep --project mokapot_coppice_hparams sweep_lgbm_hparams.yml &> param_sweep.info
+
+# Added here to support OSX where gnu grep is installed as ggrep
+if command_exists ggrep ; then
+    sweep_id=$(cat param_sweep.info | ggrep -oP "(?<=ID: ).*")
+else
+    sweep_id=$(cat param_sweep.info | grep -oP "(?<=ID: ).*")
+fi
+
+
+for i in {1..$NUM_WORKERS} ; do
+    echo "Launching worker ${i}"
+    python hparams_agent.py --sweep_id ${sweep_id} &> log_hpworker_${i}.log &
+done
+
+echo "Waiting for workers to finish..."
+wait
---
+++ benchmarking/hparams_agent.py
@@ -0,0 +1,116 @@
+import re
+import tempfile
+from pathlib import Path
+
+import mokapot
+import numpy as np
+import pandas as pd
+import wandb
+from lightgbm import LGBMClassifier
+from mokapot.model import Model
+
+MOD_REGEX = re.compile(r"\[.*?\]")
+FLANKING_REGEX = re.compile(r"(^.{1,3}\.)|(\..{1,3}$)")
+
+
+def run_model(data, model_args):
+    clf = LGBMClassifier(
+        verbose=0,
+        min_data_in_bin=10,
+        force_row_wise=True,
+        bagging_freq=10,
+        subsample_freq=10,
+        metric="cross_entropy",
+        # early_stopping_round=30,
+        **model_args
+    )
+    model = Model(clf)
+    # Read the PSMs from the PIN file:
+    psms = mokapot.read_pin(data)
+    results, models = mokapot.brew(psms, model)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        results.to_txt(dest_dir=tmpdir, decoys=True)
+        out = get_metrics(tmpdir + "/")
+    return out
+
+
+def get_metrics(outdir):
+    pep_dat = pd.concat(
+        [
+            pd.read_csv(outdir + "mokapot.decoy.peptides.txt", sep="\t"),
+            pd.read_csv(outdir + "mokapot.peptides.txt", sep="\t"),
+        ]
+    )
+    psm_dat = pd.concat(
+        [
+            pd.read_csv(outdir + "mokapot.decoy.psms.txt", sep="\t"),
+            pd.read_csv(outdir + "mokapot.psms.txt", sep="\t"),
+        ]
+    )
+
+    one_percent_peps = pep_dat["Peptide"][
+        pep_dat["Label"] * (pep_dat["mokapot q-value"] < 0.01)
+    ]
+    npep = len(one_percent_peps)
+    npsm = len(
+        psm_dat["Peptide"][psm_dat["Label"] * (psm_dat["mokapot q-value"] < 0.01)]
+    )
+    sequences = [MOD_REGEX.sub("", x) for x in one_percent_peps]
+    sequences = [FLANKING_REGEX.sub("", x) for x in sequences]
+    nseq = len(set(sequences))
+
+    return {"npep_1pct": npep, "npsm_1pct": npsm, "nseq_1pct": nseq, "table": pep_dat}
+
+
+def main():
+    run = wandb.init(project="mokapot_coppice_hparams")
+    artifact = run.use_artifact("jspaezp/mokapot_coppice/pinfiles:v0", type="raw_data")
+    artifact_dir = artifact.download()
+    DATASETS = list(Path(artifact_dir).rglob("*.pin"))
+
+    model_args = {
+        "learning_rate": wandb.config.lr,
+        "max_depth": wandb.config.max_depth,
+        "num_leaves": wandb.config.num_leaves,
+        "feature_fraction": wandb.config.feature_fraction,
+        "colsample_bytree": wandb.config.feature_fraction,
+        "data_sample_strategy": wandb.config.data_sample_strategy,
+        "subsample": wandb.config.subsample,  # bagging is an alias
+        "boosting": wandb.config.boosting,
+        "boosting_type": wandb.config.boosting,
+        "num_iterations": wandb.config.num_iterations,
+    }
+
+    results = {}
+    for data in DATASETS:
+        dataname = data.stem
+        out = run_model(str(data), model_args=model_args)
+        out.pop("table")
+
+        # pep_dat = out.pop("table")
+        # scores = [pep_dat["mokapot score"], 1-pep_dat["mokapot score"]]
+        # targets = ["Target" if x else "Decoy" for x in pep_dat["Label"].array]
+        # roc = wandb.plot.roc_curve(targets, np.array(scores).T)
+        # wandb.log({"roc": roc})
+
+        results[dataname] = out
+        out = {dataname + k: v for k, v in out.items()}
+        wandb.log(out)
+
+        # TODO add acceptances vs FDR plot
+
+    mean_val = np.mean([x["npep_1pct"] for x in results.values()])
+    wandb.log({"mean_npep_1pct": mean_val})
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sweep_id", type=str)
+    args = parser.parse_args()
+
+    # Start sweep job.
+    wandb.agent(
+        args.sweep_id, function=main, project="mokapot_coppice_hparams", count=20
+    )
